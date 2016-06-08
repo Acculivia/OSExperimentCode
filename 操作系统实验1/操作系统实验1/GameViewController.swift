f72106755a6a5f7a01736567a5a04a14125d3cba
Hilovaiserillis <ygdvv1@qq.com>
2016-06-08 12:31:34 +0800

    .

--- 操作系统实验1/操作系统实验1/GameViewController.swift
+++ 操作系统实验1/操作系统实验1/GameViewController.swift
@@ -6,7 +6,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var scene = GameScene(size: CGSizeMake(1600, 900))
+        let scene = GameScene(size: CGSizeMake(1600, 900))
         // Configure the view.
         let skView = self.view as! SKView
         skView.showsFPS = true
